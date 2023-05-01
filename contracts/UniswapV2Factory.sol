f4649e3d62c6e1acd0365bd1d1053067a0d357de
slowisfast <slowisfast2030@163.com>
2023-05-01 16:52:10 +0800

    slow is fast

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -5,8 +5,8 @@ import './UniswapV2Pair.sol';
 
 // factory contract主要目的是创建流动性池子，每个流动性池子都是一个pair合约
 contract UniswapV2Factory is IUniswapV2Factory {
-    address public feeTo;
-    address public feeToSetter;
+    address public feeTo; // 收税地址（0.005%的协议费用，到目前为止uniswap也没有设置）
+    address public feeToSetter; // 收税权限地址
 
     // 前两个地址分别对应交易对中的两种代币地址，最后一个地址是交易对合约本身地址
     // 疑问：代币的地址是什么？
@@ -28,19 +28,36 @@ contract UniswapV2Factory is IUniswapV2Factory {
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
         require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
-        // arranges the two input tokens in ascending order while assigning them to respective variables.
+        // tA 和 tB 进行排序，确保tA小于tB。返回对应的t0和t1
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        
+        // // 判断tA 地址不为0，继而判断出tB 地址也不为0
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        // checks if there isn't already another UniswapV2 pair created with the same tokens.
+        
+        // 校验tA 和 tB 的地址不存在配对过
         require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
-        // defines a memory variable called bytecode as the compiled contract creation code for UniswapV2Pair.
+        
+        // 通过引入UniswapV2Pair合约，不使用继承的方式。
+        // 使用type(合约名称).creationCode 方法获得该合约编译之后的字节码
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
+
+        // abi.encodePacked()     编码打包
+        // keccak256              Solidity 内置加密Hash方法
+        // keccak256(abi.encodePacked(a, b))是计算keccak256(a, b)更明确的方式
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        // the create2() EVM opcode is used to deploy a new instance of UniswapV2Pair. The new pair is stored in the pair variable.
-        assembly {
+        
+        // 内联汇编
+        // mload(bytecode)          返回长度
+        // create2                  新的操作码 （opcode 操作码是程序的低级可读指令, 所有操作码都具有对应的十六进制值）
+        assembly {                  // 使用汇编Opcode来操作EVM字节码，可以节省gas和做一些无法通过Solidity完成的事情
+        //通过create2方法布署合约,并且加盐,返回地址到pair变量
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
+
+        // 调用pair合约的初始化方法，传入参数tA tB
         IUniswapV2Pair(pair).initialize(token0, token1);
+
+        // pair是合约地址
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
@@ -48,11 +65,13 @@ contract UniswapV2Factory is IUniswapV2Factory {
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
 
+    // 设置收税地址
     function setFeeTo(address _feeTo) external {
         require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
         feeTo = _feeTo;
     }
 
+    // 设置收税权限
     function setFeeToSetter(address _feeToSetter) external {
         require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
         feeToSetter = _feeToSetter;
