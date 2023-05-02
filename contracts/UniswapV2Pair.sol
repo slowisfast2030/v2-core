89483e8c0b3b9780f798cfa10007e3ce774054fa
slowisfast <slowisfast2030@163.com>
2023-05-02 13:53:21 +0800

    slow is fast

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -185,6 +185,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
 
     // this low-level function should be called from a contract which performs important safety checks
     // 传入的参数amount0Out，amount1Out,to以及data分别是要购买的token0的数量，token1的数量，接收者的地址，接收后执行回调传递数据。
+    // 困惑的点：这里为何有两个amountOut？
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
         // token0和token1哪一个是要购买的token？好问题！
         /**
