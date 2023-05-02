4191ce20243cfbfd43ba89f2c1d482ebc2dfc956
slowisfast <slowisfast2030@163.com>
2023-05-02 13:25:35 +0800

    slow is fast

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -208,21 +208,25 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
 
-        // 这一步已经完成给msg.sender转账了？
+        // 闪电贷
         if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
 
+        // 获取最新的t0和t1余额
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
 
+        // amountIn = balance - (_reserve - amountOut)
+        // 根据取出的储备量、原有储备量以及最新的余额，反推得到输入的数额
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
 
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+        // 调整后的余额 = 最新余额 - 扣税金额 （相当于乘以997/1000）
         uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
         uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-        // 更新恒定乘积公式，并且新的值要大于等于原来的值。
+        // 校验是否进行了扣税计算
         require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
         }
 
